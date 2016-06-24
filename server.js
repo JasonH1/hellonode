9e119fb98b1c434d39485807e2638d33edfd7951
d8ii <d8ii@d8iis-MacBook-Pro.local>
2016-06-24 16:32:44 +0700

    example files

---
+++ server.js
@@ -0,0 +1,12 @@
+#!/bin/env node
+
+var express = require('express'),
+	app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
